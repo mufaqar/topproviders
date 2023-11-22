5e58efb59a60afa15a6221465967be4ecb8d33c6
mufaqar <mufaqar@gmail.com>
2023-11-22 11:22:43 +0500

    comapir

---
+++ src/pages/comparisons/index.tsx
@@ -0,0 +1,26 @@
+import Main from '@/components/main'
+import { ProviderBox } from '@/components/provider/provider-box'
+import apolloClient from '@/config/client'
+import { GET_ALL_PROVIDERS } from '@/config/query'
+import { GetStaticProps } from 'next'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import { FaArrowRightLong } from 'react-icons/fa6'
+import Head from 'next/head';
+function Comparsons({ allProviders }: any) {
+
+    return (
+        <>
+            
+            Comprapre
+
+
+           
+        </>
+    )
+}
+
+export default Comparsons
+
+
