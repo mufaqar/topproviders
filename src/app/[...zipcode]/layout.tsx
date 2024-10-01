0e97aca81b2f8c86847d8014554d8e00d2c30d0d
syed-zunair-gillani <dev.zunaair@gmail.com>
2024-10-01 15:58:56 +0500

    add scripts in city, state and zipcode pages

--- src/app/[...zipcode]/layout.tsx
+++ src/app/[...zipcode]/layout.tsx
@@ -1,14 +1,13 @@
-import { Metadata } from "next";
 import React from "react";
 import { openGraph, twitter } from "@/lib/seoMeta";
 import { fetchStateData } from "@/utils/fetchStateData";
 
 import { fetchCityData } from "@/utils/fetchCityData";
 import { fetchZipcodeData } from "@/utils/fetchZipcodeData";
-import { genMetaDescription } from "@/utils/genMetaDescription";
+import { genFullMetaDescription, genMetaDescription } from "@/utils/genMetaDescription";
+import Script from "next/script";
 
 export async function generateMetadata({ params }: any) {
-    
   // SEO meta for states
   if (params?.zipcode.length === 2 && params?.zipcode?.[1].length === 2) {
     const stateResult: any = await fetchStateData(params?.zipcode);
@@ -18,7 +17,7 @@ export async function generateMetadata({ params }: any) {
     return {
       title: `Top ${stateResult?.StateData?.providers?.length} ${type} service provider in ${state}`,
       description: genMetaDescription({
-        providers : stateResult?.StateData?.providers, 
+        providers: stateResult?.StateData?.providers,
         state: state,
         type,
       }),
@@ -40,7 +39,7 @@ export async function generateMetadata({ params }: any) {
     return {
       title: `Top ${cityResult?.CityData?.providers.length} ${type} Service Provider in ${city}`,
       description: genMetaDescription({
-        providers : cityResult?.CityData?.providers, 
+        providers: cityResult?.CityData?.providers,
         city,
         state,
         type,
@@ -65,9 +64,9 @@ export async function generateMetadata({ params }: any) {
     return {
       title: `Top ${zipcodeResult?.ZipData?.length} ${type} service provider in ${zipcode}`,
       description: genMetaDescription({
-        providers : zipcodeResult?.ZipData, 
+        providers: zipcodeResult?.ZipData,
         type,
-        zipcode: zipcode.replace("zipcode-", "")
+        zipcode: zipcode.replace("zipcode-", ""),
       }),
       alternates: {
         canonical: `https://www.topproviders.net/${type}/zipcode-${zipcode}`,
@@ -78,8 +77,143 @@ export async function generateMetadata({ params }: any) {
   }
 }
 
-const ZipLayout = ({ children }: any) => {
-  return <>{children}</>;
+
+
+async function fetchCityDataAsync(query: any) {
+  const cityResult = await fetchCityData(query);
+  return {
+    CityData: cityResult?.CityData,
+    cityNotFound: cityResult?.cityNotFound,
+  };
+}
+
+async function fetchStateDataAsync(query: any) {
+  const stateResult = await fetchStateData(query);
+  return {
+    StateData: stateResult?.StateData,
+    allcities: stateResult?.allcities,
+    state: stateResult?.state,
+    stateNotFound: stateResult?.stateNotFound,
+  };
+}
+
+async function fetchZipcodeDataAsync(query: any) {
+  const zipcodeResult = await fetchZipcodeData(query);
+  return {
+    ZipData: zipcodeResult.ZipData,
+    zipcode: zipcodeResult.zipcode,
+    type: zipcodeResult.type,
+    zipNotFound: zipcodeResult.zipNotFound,
+  };
+}
+
+
+const ZipLayout = async (props:any) => {
+  const query = props?.params?.zipcode
+  const { CityData, cityNotFound } = await fetchCityDataAsync(query);
+  const { StateData, allcities, state, stateNotFound } = await fetchStateDataAsync(query);
+  const { ZipData, zipcode, type, zipNotFound } = await fetchZipcodeDataAsync(query);
+  
+  const images = CityData?.providers?.map((item:any)=>item?.featured_image)
+  const stateImages = StateData?.providers?.map((item:any)=>item?.featured_image)
+  const zipImages = ZipData?.map((item:any)=>item?.featuredImage).map((i:any)=>i.node?.mediaItemUrl)
+
+  const scriptContent = `
+    <script type="application/ld+json">
+      {
+        "@context": "http://schema.org",
+        "@type": "InternetServiceProvider",
+        "name": "${query?.[0]} servise providers in ${query?.[1]}, ${query?.[2]}",
+        "image": ${images},
+        "description": "Discover the perfect Internet and TV Service Providers. Find unbeatable deals on Cable and High-Speed Internet to make your move seamless and enjoyable!",
+        "url": "https://www.topproviders.net/${query?.[0]}/${query?.[1]}/${query?.[2]}",
+        "brand": {
+          "@type": "Brand",
+          "name": "Top Providers",
+          "logo": "https://www.topproviders.net/_next/image?url=%2Flogo.png&w=256&q=75"
+        },
+        "aggregateRating": {
+          "@type": "AggregateRating",
+          "ratingValue": "4.5",
+          "bestRating": "5",
+          "ratingCount": "120"
+        }
+      }
+    </script>
+  `;
+
+  const stateScriptContent = `
+    <script type="application/ld+json">
+      {
+        "@context": "http://schema.org",
+        "@type": "InternetServiceProvider",
+        "name": "${query?.[0]} servise providers in ${query?.[1]}",
+        "image": ${stateImages},
+        "description": ${
+          genFullMetaDescription({
+            providers: StateData?.providers,
+            state: query?.[1],
+            type: query?.[0],
+          })
+        },
+        "url": "https://www.topproviders.net/${query?.[0]}/${query?.[1]}",
+        "brand": {
+          "@type": "Brand",
+          "name": "Top Providers",
+          "logo": "https://www.topproviders.net/_next/image?url=%2Flogo.png&w=256&q=75"
+        },
+        "aggregateRating": {
+          "@type": "AggregateRating",
+          "ratingValue": "4.5",
+          "bestRating": "5",
+          "ratingCount": "120"
+        }
+      }
+    </script>
+  `;
+
+  const zipcodeScriptContent = `
+    <script type="application/ld+json">
+      {
+        "@context": "http://schema.org",
+        "@type": "InternetServiceProvider",
+        "name": "${query?.[0]} servise providers in zipcode ${query?.[1].replace("zipcode-", "")}",
+        "image": ${zipImages},
+        "description": ${
+          genFullMetaDescription({
+            providers: ZipData,
+            type: query?.[0],
+            zipcode: query?.[1].replace("zipcode-", ""),
+          })
+        },
+        "url": "https://www.topproviders.net/${query?.[0]}/${query?.[1]}",
+        "brand": {
+          "@type": "Brand",
+          "name": "Top Providers",
+          "logo": "https://www.topproviders.net/_next/image?url=%2Flogo.png&w=256&q=75"
+        },
+        "aggregateRating": {
+          "@type": "AggregateRating",
+          "ratingValue": "4.5",
+          "bestRating": "5",
+          "ratingCount": "120"
+        }
+      }
+    </script>
+  `;
+
+
+  const currenScript = CityData ? scriptContent : StateData ? stateScriptContent : ZipData && zipcodeScriptContent
+
+
+  return (
+    <>
+      <head>
+        <Script dangerouslySetInnerHTML={{ __html: currenScript }} />
+      </head>
+      {props?.children}
+    </>
+  );
 };
 
 export default ZipLayout;
--- src/utils/genMetaDescription.ts
+++ src/utils/genMetaDescription.ts
@@ -10,4 +10,12 @@ export const genMetaDescription = ({ city, state, type,  providers, zipcode}:any
     return description
   };
   
-  
+  export const genFullMetaDescription = ({ city, state, type,  providers, zipcode}:any) => {
+    const formattedTitles = () => {
+      return providers?.map((provider:any, index:number) => `${(index + 1).toString().padStart(2, '0')}- ${provider.title}`).join(', ');
+    };
+    let description = `Top ${providers?.length} ${type} Service Providers in ${zipcode ? zipcode : ""} ${city ? city : ""}${city ? "," : ""} ${state ? state : ""} are ${formattedTitles()} `;
+    // If the description exceeds 155 characters, truncate and add "..." after 152 characters
+
+    return description
+  };
